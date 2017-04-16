64e6f7a4c259a31b716f4fe9030c03c02318cdcb
Airspin <airspin1575@yandex.ru>
2017-04-17 00:11:26 +0300

    add redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
 import App from './App';
 import './index.css';
 
+
+const store = createStore(reducer,)
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <Provider>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 );
